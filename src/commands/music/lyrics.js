78b6c64fa0a42a56167388c5ef18e99b7b3133fd
Annas Bintar <abxpendz45@gmail.com>
2024-01-24 19:00:43 +0700

    Fixing bug and Updating packages

--- src/commands/music/lyrics.js
+++ src/commands/music/lyrics.js
@@ -24,7 +24,6 @@ module.exports = {
     if (search.length === 0) {
       return interaction.reply({ content: "❌  |  Can't find lyrics! Try a more specific search term.", ephemeral: true });
     }
-    console.log(search);
     const song = search[0];
 
     try {
