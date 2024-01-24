f298ae8e452cd190309ecf3495792bfa28fbbeb9
Annas Bintar <abxpendz45@gmail.com>
2024-01-24 22:15:36 +0700

    Fixing bugs

--- src/commands/etc/segss.js
+++ src/commands/etc/segss.js
@@ -10,20 +10,32 @@ module.exports = {
         .setName("tags")
         .setDescription("Tags of segss")
         .setRequired(true)
+    )
+    .addStringOption(
+      opt => opt
+        .setName("category")
+        .setDescription("Type of image")
+        .setRequired(true)
+        .addChoices(
+          { name : "General", value : "general" },
+          { name : "Questionable", value : "questionable" },
+          { name : "Explicit", value : "explicit" }
+        )
     ),
 
   async execute(client, interaction) {
     try {
       const tags = interaction.options.getString("tags");
-
-      const response = await axios.get(`https://gelbooru.com/index.php?page=dapi&s=post&q=index&tags=${tags}&limit=50&json=1`);
+      const cat = interaction.options.getString("category");
+      
+      const response = await axios.get(`https://gelbooru.com/index.php?page=dapi&s=post&q=index&tags=${tags}&json=1`);
 
       let imageUrl
       let j = 0;
-      while (response.data.post[j].rating !== "explicit") {
-        j = Math.floor(Math.random() * 50);
+      while (response.data.post[j].rating !== cat) {
+        j = Math.floor(Math.random() * 100);
 
-        if (response.data.post[j].rating == "explicit") {
+        if (response.data.post[j].rating === cat) {
           imageUrl = response.data.post[j].file_url;
           break;
         }
@@ -32,7 +44,7 @@ module.exports = {
       // Send the image URL as a message
       let embed = new EmbedBuilder()
         .setTitle("Result Images")
-        .setImage(imageUrl)
+        .setImage(imageUrl ? imageUrl : "Can't Find the image!")
         .setColor("Blue")
         .setTimestamp(Date.now());
 
