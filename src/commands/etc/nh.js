0c18a45f17bf69323a66a4736537b6ff44aba5ae
Annas Bintar <abxpendz45@gmail.com>
2024-08-18 19:57:05 +0700

    feat: add new commands!

---
+++ src/commands/etc/nh.js
@@ -0,0 +1,90 @@
+const { SlashCommandBuilder, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require("discord.js");
+const { API } = require("nhentai-api");
+
+let index, imageData, userid;
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("nh")
+    .setDescription("Show the book of nhen*")
+    .addStringOption(opt => opt
+      .setName("query")
+      .setDescription("Number codes or title of book")
+      .setRequired(true)
+    ),
+
+  async execute(client, interaction) {
+    if (client.config.commands.etc.nh === 0) {
+      return interaction.reply({ content: "❌  |  This command is disabled.", ephemeral: true });
+    }
+
+    const memberRoles = interaction.member.roles;
+    const roles = client.config.explicit.roles.id;
+
+    await interaction.deferReply({ ephemeral: true });
+
+    const query = interaction.options.getString("query");
+    const api = new API();
+
+    let access = false, i = 0;
+    while (i < roles.length) {
+      if (memberRoles.cache.has(roles[i])) {
+        access = true;
+        break;
+      }
+      i++;
+    }
+
+    if (!access) {
+      return interaction.reply({ content: "❌  |  You dont have permissions to run this roles", ephemeral: true });
+    }
+
+    userid = interaction.user.id;
+
+    index = 0;
+
+    await api.getBook(query).then((book) => {
+      imageData = book.pages;
+    }).catch(() => {
+      return interaction.reply({ content: "❌  |  Book not found!", ephemeral: true });
+    });
+
+    const nextButton = new ButtonBuilder()
+      .setCustomId("next-book")
+      .setLabel("Next")
+      .setStyle(ButtonStyle.Primary);
+
+    const prevButton = new ButtonBuilder()
+      .setCustomId("prev-book")
+      .setLabel("Prev")
+      .setStyle(ButtonStyle.Secondary);
+
+    const button = new ActionRowBuilder().addComponents(prevButton, nextButton);
+
+    let embed = new EmbedBuilder()
+      .setTitle("Book Results")
+      .setDescription(api.getImageURL(imageData[0]))
+      .setImage(api.getImageURL(imageData[0]))
+      .setColor("Blue")
+      .setFooter({ text: `Page ${index + 1} of ${imageData.length}` })
+      .setTimestamp(Date.now())
+
+    await interaction.editReply({ embeds: [embed], components: [button] });
+  },
+
+  getData: () => {
+    return imageData;
+  },
+
+  getUserID: () => {
+    return userid;
+  },
+
+  getIndex: () => {
+    return index;
+  },
+
+  setIndex: (i) => {
+    index = i;
+  }
+}
---
+++ src/components/buttons/next-book.js
@@ -0,0 +1,29 @@
+const { EmbedBuilder } = require("discord.js");
+const { API } = require("nhentai-api");
+const { getIndex, getData, setIndex, getUserID } = require("../../commands/etc/nh");
+
+module.exports = {
+  data: { name: "next-book" },
+
+  async execute(client, interaction) {
+    const api = new API();
+
+    let imageData = getData();
+    let i = getIndex();
+
+    if (interaction.user.id !== getUserID()) return interaction.reply({ content : "❌  |  You're not allowed to use this button!", ephemeral : true });
+    let index = i + 1;
+    if (index > imageData.length - 1) index = 0;
+    setIndex(index);
+
+    let embed = new EmbedBuilder()
+      .setTitle("Book Results")
+      .setDescription(api.getImageURL(imageData[index]))
+      .setImage(api.getImageURL(imageData[index]))
+      .setColor("Blue")
+      .setFooter({ text: `Page ${index + 1} of ${imageData.length}` })
+      .setTimestamp(Date.now())
+
+    await interaction.update({ embeds: [embed], content: "" });
+  }
+}
