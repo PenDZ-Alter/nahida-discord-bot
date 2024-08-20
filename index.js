f717485b9eb52db31fb601ee11dc0b7ecdb0c0e3
Annas Bintar <abxpendz45@gmail.com>
2024-08-20 12:06:14 +0700

    style: simplify code

--- index.js
+++ index.js
@@ -14,12 +14,7 @@ client.commandsData = [];
 client.player = new Player(client);
 
 if (client.config.debug) {
-  if (client.config.debug === "player" || client.config.debug === "client" || client.config.debug === "all") {
-    console.log("BOT :: Debug level = " + client.config.debug);
-  }
-  else {
-    console.log("BOT :: Debug level = N/A");
-  }
+  console.log(`BOT :: Debug level = ${["player", "client", "all"].includes(client.config.debug) ? client.config.debug : "N/A"}`);
 }
 
 // File Listeners
