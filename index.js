2dad15b5e6d99b87f7839ab24d1feea169d70596
Annas Bintar <abxpendz45@gmail.com>
2024-08-21 23:52:23 +0700

    chore: remove debug token

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("dotenv").config({ path: "./config/.env" });
 
 const client = new Client(clientSettings());
 
-client.config = require("./config/config.test.json");
+client.config = require("./config/config.json");
 
 // Properties of data
 client.commands = new Collection();
