3ae9f9095335df13008a27c1aec973698c2c8a85
PenDZ-Alter <abxpendz45@gmail.com>
2025-04-17 15:16:34 +0700

    feat(cmd): add skip on player commands

--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -1,13 +1,13 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const { joinVoiceChannel } = require("@discordjs/voice");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("play")
-    .setDescription("Mainin lagu dari YouTube")
+    .setDescription("Playing song from youtube")
     .addStringOption(option =>
       option.setName("query")
-        .setDescription("Judul atau URL lagu")
+        .setDescription("Query of song")
         .setRequired(true)),
 
   async execute(client, interaction) {
@@ -15,9 +15,9 @@ module.exports = {
     const member = interaction.member;
 
     const voiceChannel = member.voice.channel;
-    if (!voiceChannel) return interaction.reply({ content: "❌ Lo harus join voice channel dulu!", ephemeral: true });
+    if (!voiceChannel) return interaction.reply({ content: "❌ Join voice channel first!", ephemeral: true });
 
-    await interaction.deferReply();
+    await interaction.deferReply({ ephemeral: true });
 
     const player = await client.kazagumo.createPlayer({
       guildId: interaction.guild.id,
@@ -27,16 +27,33 @@ module.exports = {
     });
 
     const result = await client.kazagumo.search(query, { requester: interaction.user });
-    if (!result.tracks.length) return interaction.editReply("⚠️ Gagal cari lagu.");
+    if (!result.tracks.length) return interaction.editReply("⚠️ Failed to get song. Try more specific!");
 
-    if (result.type === "PLAYLIST") {
-      for (const track of result.tracks) player.queue.add(track);
+    let totalSong, title;
+    if (result.type == "PLAYLIST") {
+      for (const track of result.tracks) {
+        player.queue.add(track);
+        totalSong += 1;
+      }
       if (!player.playing) player.play();
-      return interaction.editReply(`📜 Playlist ditambahin: **${result.playlistName}**`);
+      // return interaction.editReply(`📜 Playlist added: **${result.playlistName}**`);
     } else {
       player.queue.add(result.tracks[0]);
       if (!player.playing) player.play();
-      return interaction.editReply(`🎧 Lagu ditambahin: **${result.tracks[0].title}**`);
+      title = result.tracks[0].title;
+      // return interaction.editReply(`🎧 Song Added: **${result.tracks[0].title}**`);
     }
+
+    let songIndex = player.queue.size;
+
+    let embed = new EmbedBuilder()      
+      .setTitle("Playback Information")
+      .setColor("Blue")
+      .setDescription(
+        `📝  |  **${title}** has been enqueued!
+        ℹ️  |  Source : ${result.type == "SEARCH" ? "Tracks" : "Playlist"}
+        ℹ️  |  ${result.type == "SEARCH" ? `Track Status : ${songIndex === 0 ? "Playing right now!" : `Added in position ${songIndex}`}` : `Total song indexed : ${totalSong}`}`);
+
+    await interaction.editReply({ embeds: [embed] });
   },
 };
---
+++ src/commands/music/skip.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { joinVoiceChannel } = require("@discordjs/voice");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("skip")
+    .setDescription("Go to the next song."),
+
+  async execute(client, interaction) {
+    const player = client.kazagumo.players.get(interaction.guild.id);
+    if (!player || !player.playing) return interaction.reply("❌ No song are playing.");
+
+    let queue = player.queue;
+    let currentSong = queue.current.title;
+    let nextSong = queue[0].title;
+
+    player.skip();
+
+    let embed = new EmbedBuilder()
+      .setTitle("Playback Information")
+      .setColor("Blue")
+      .setDescription(`⏭️ The song **${currentSong}** has been skipped!\n🎵 ${nextSong ? `Now Playing **${nextSong}**` : "The Player Has Stopped!"}`);
+
+    await interaction.reply({ embeds : [embed] });
+  },
+};
