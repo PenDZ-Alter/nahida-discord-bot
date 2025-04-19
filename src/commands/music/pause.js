abf5a2b85fbf7a8f5225d0cf1343337f31e76a30
PenDZ-Alter <abxpendz45@gmail.com>
2025-04-19 19:42:03 +0700

    feat: add pause commands

---
+++ src/commands/music/pause.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("pause")
+    .setDescription("Pause or resume the song"),
+
+  async execute(client, interaction) {
+    const player = client.kazagumo.players.get(interaction.guild.id);
+
+    const channel = interaction.member.voice.channel;
+    if (!channel) return interaction.reply({ content : '❌  |  You are not connected to a voice channel!', ephemeral : true});
+
+    if (!interaction.member.voice.channel) return interaction.reply({ content: "❌  |  You must join vc first!", ephemeral: true });
+    if (interaction.guild.members.me.voice.channel && interaction.member.voice.channel.id !== interaction.guild.members.me.voice.channel.id) {
+      return interaction.reply({ content: "❌  |  You must join in same vc to request song!", ephemeral: true })
+    }
+
+    if (!player) return interaction.reply({ content : "❌  |  You're not playing music rn!", ephemeral : true });
+
+    if (player.paused) {
+      await player.pause(false);
+      return interaction.reply({ content : "✅  |  Resumed the song!", ephemeral : false });
+    } else {
+      await player.pause(true);
+      return interaction.reply({ content : "✅  |  Paused the song!", ephemeral : false });
+    }
+
+  },
+};
--- src/commands/music/queue.js
+++ src/commands/music/queue.js
@@ -51,8 +51,10 @@ module.exports = {
   },
   formatDuration: (ms) => {
     const totalSeconds = Math.floor(ms / 1000);
-    const minutes = Math.floor(totalSeconds / 60);
+    const totalMinutes = Math.floor(totalSeconds / 60);
+    const hours = Math.floor(totalMinutes / 60);
+    const minutes = totalMinutes % 60;
     const seconds = totalSeconds % 60;
-    return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+    return `${!hours ? "" : hours}${!hours ? "" : ":"}${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
   }
 };
