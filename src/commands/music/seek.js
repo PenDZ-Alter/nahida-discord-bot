df250d78480378ef5a147376d1872df772268907
PenDZ-Alter <abxpendz45@gmail.com>
2025-06-26 21:34:54 +0700

    feat(cmd): adding `seek` in player

---
+++ src/commands/music/seek.js
@@ -0,0 +1,75 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("seek")
+    .setDescription("Go to the next song with position based on queue.")
+    .addIntegerOption(opt => opt
+      .setName("sec")
+      .setDescription("Position in seconds")
+      .setRequired(true)
+    )
+    .addIntegerOption(opt => opt
+      .setName("min")
+      .setDescription("Position in minutes")
+      .setRequired(false)
+    )
+    .addIntegerOption(opt => opt
+      .setName("hrs")
+      .setDescription("Position in hours")
+      .setRequired(false)
+    ),
+
+  async execute(client, interaction) {
+    if (client.config.commands.music.seek === 0) {
+      return interaction.reply({ content: "❌  |  This command is disabled.", ephemeral: true });
+    }
+
+    const player = client.kazagumo.players.get(interaction.guild.id);
+    let queue = player.queue;
+
+    const sec = interaction.options.getInteger("sec");
+    const min = interaction.options.getInteger("min");
+    const hrs = interaction.options.getInteger("hrs");
+
+    if (sec > 60) {
+      return interaction.reply({ content : "❌  |  Max value of seconds is 60", ephemeral : true });
+    }
+
+    if (min > 60) {
+      return interaction.reply({ content : "❌  |  Max value of minutes is 60", ephemeral : true });
+    }
+
+    if (hrs > 24) {
+      return interaction.reply({ content : "❌  |  Max value of hours is 24", ephemeral : true });
+    }
+
+    const channel = interaction.member.voice.channel;
+    if (!channel) return interaction.reply({ content: '❌  |  You are not connected to a voice channel!', ephemeral: true });
+
+    if (interaction.guild.members.me.voice.channel && interaction.member.voice.channel.id !== interaction.guild.members.me.voice.channel.id) {
+      return interaction.reply({ content: "❌  |  You must join in same vc to request song!", ephemeral: true });
+    }
+
+    if (!player || !player.playing) return interaction.reply("❌  |  No song are playing.");
+
+    let durationTime = sec + (min * 60) + (hrs * 3600);
+
+    let fmin;
+    if (min < 10) fmin = "0" + min
+    else fmin = min;
+
+    let fsec;
+    if (sec < 10) fsec = "0" + sec
+    else fsec = sec;
+
+    player.seek(durationTime);
+    
+    const embed = new EmbedBuilder()
+      .setTitle("Playback Information")
+      .setColor("Blue")
+      .setDescription(`✅  |  Seeked to ${!hrs ? "0" : hrs}:${!min ? "00" : fmin}:${sec === 0 ? "00" : fsec}!`);
+
+    await interaction.reply({ embeds: [embed] });
+  },
+};
